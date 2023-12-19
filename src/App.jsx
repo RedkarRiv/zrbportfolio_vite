636f5f8bb957f59a0a5d5b57970f9d9c50f52560
Carlos <carresan.val@gmail.com>
2023-12-20 00:38:32 +0100

    TEST: Probando diseños de Home. DegradadoRosa v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { NavbarTop } from "./components/Navbar";
 import { Body } from "./routing/Body";
 
 export const App = () => {
   return (
     <>
+      <NavbarTop />
       <Body />
     </>
   );
-}
-
-
+};
